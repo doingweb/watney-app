5bd76139ecf258d45b7477d62a8c315238a11d03
Chris Antes <chris@chrisdoingweb.com>
2017-05-26 22:02:15 -0700

    Add Logger factory for Scripts
    New util function for creating a Logger for Scripts. Way more convenient than having to get the basename (or make up a name) in each script.

--- src/util.js
+++ src/util.js
@@ -1,3 +1,22 @@
+const path = require('path'),
+  Logger = require('./Logger');
+
+/**
+ * Pauses execution for a specified period of time.
+ *
+ * @param {number} ms The number of milliseconds to wait.
+ * @returns {Promise} An awaitable Promise for the end of the pause.
+ */
 module.exports.sleep = function sleep (ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+/**
+ * Creates a new Logger for a script with the given filename.
+ *
+ * @param {string} filename The name of the script file (with extension).
+ * @returns A new Logger for the script.
+ */
+module.exports.createScriptLogger = function createScriptLogger (filename) {
+  return new Logger(path.basename(filename, '.js'));
 };
