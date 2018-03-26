8fdd4f550d827d2ed38635c0e7e949f182475b0b
Chris Antes <chris@chrisdoingweb.com>
2018-03-26 01:26:31 -0700

    First take at WatneyApp class
    Starts on an idea for a pluggable object-oriented app runner that can be installed via a package manager and instantiated with plugins (e.g., Hue, Nest, etc.) and scripts (e.g., "turn up the porch light when somebody is on the porch").

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  WatneyApp: require('./lib/WatneyApp'),
+  WatneyPlugin: require('./lib/WatneyPlugin'),
+  WatneyScript: require('./lib/WatneyScript'),
+  WatneyTool: require('./lib/WatneyTool')
+};
