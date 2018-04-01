0056dbcd7caf98589ab678d7f3662923cd8b4fe0
Chris Antes <chris@chrisdoingweb.com>
2018-04-01 13:50:46 -0700

    Add CLI feature

---
+++ bin/watney.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+
+const path = require('path');
+const app = require(path.join(process.cwd(), 'app'));
+
+const pluginKeys = Array.from(app.plugins.keys());
+
+const selectPlugin = {
+  name: 'pluginId',
+  message: 'Run command-line interface for plugin',
+  type: 'list',
+  choices: [...pluginKeys, new inquirer.Separator(), 'Exit']
+};
+
+(async function main() {
+  while (true) {
+    let { pluginId } = await inquirer.prompt(selectPlugin);
+
+    if (pluginId === 'Exit') {
+      return;
+    }
+
+    let plugin = app.plugins.get(pluginId);
+    await plugin.constructor.cli(app);
+
+    let { returnToMenu } = await inquirer.prompt({
+      name: 'returnToMenu',
+      type: 'confirm',
+      message: 'Return to plugins menu?',
+      default: true
+    });
+
+    if (!returnToMenu) {
+      return;
+    }
+  }
+})();
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   WatneyApp: require('./lib/WatneyApp'),
   WatneyPlugin: require('./lib/WatneyPlugin'),
-  WatneyScript: require('./lib/WatneyScript'),
-  WatneyTool: require('./lib/WatneyTool')
+  WatneyScript: require('./lib/WatneyScript')
 };
